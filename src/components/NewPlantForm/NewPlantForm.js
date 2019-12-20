12f64c12df85e5b7fc017f8e58f055eeee043eb4
lukemm23 <lukemm23@gmail.com>
2019-12-20 14:07:43 -0600

    material button and text field

--- src/components/NewPlantForm/NewPlantForm.js
+++ src/components/NewPlantForm/NewPlantForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const mapStateToProps = reduxState => ({
     reduxState,
@@ -9,10 +11,10 @@ class NewPlantForm extends Component {
     state = {
         newPlant: {
             name: '',
-            kingdom:'',
+            kingdom: '',
             clade: '',
             order: '',
-            family:'',
+            family: '',
             subfamily: '',
             genus: '',
         }
@@ -38,15 +40,17 @@ class NewPlantForm extends Component {
             <div>
                 <h3>This is the form</h3>
                 <pre>{JSON.stringify(this.state)}</pre>
-                onSubmit={this.addNewPlant}
-                    <input type='text' value={this.state.newPlant.name} placeholder="name" onChange={(event)=>this.handleNameChange(event, 'name')} />
-                    <input type='text' value={this.state.newPlant.kingdom} placeholder="kingdom" onChange={(event)=>this.handleNameChange(event, 'kingdom')} />
-                    <input type='text' value={this.state.newPlant.clade} placeholder="clade" onChange={(event)=>this.handleNameChange(event, 'clade')} />
-                    <input type='text' value={this.state.newPlant.order} placeholder="order" onChange={(event)=>this.handleNameChange(event, 'order')} />
-                    <input type='text' value={this.state.newPlant.family} placeholder="family" onChange={(event)=>this.handleNameChange(event, 'family')} />
-                    <input type='text' value={this.state.newPlant.subfamily} placeholder="subfamily" onChange={(event)=>this.handleNameChange(event, 'subfamily')} />
-                    <input type='text' value={this.state.newPlant.genus} placeholder="genus" onChange={(event)=>this.handleNameChange(event, 'genus')} />
-                    <button onClick={this.addNewPlant}>submit</button>
+                <div>
+                    <TextField type='text' value={this.state.newPlant.name} placeholder="name" onChange={(event) => this.handleNameChange(event, 'name')} />
+                    <TextField type='text' value={this.state.newPlant.kingdom} placeholder="kingdom" onChange={(event) => this.handleNameChange(event, 'kingdom')} />
+                    <TextField type='text' value={this.state.newPlant.clade} placeholder="clade" onChange={(event) => this.handleNameChange(event, 'clade')} />
+                    <TextField type='text' value={this.state.newPlant.order} placeholder="order" onChange={(event) => this.handleNameChange(event, 'order')} />
+                    <TextField type='text' value={this.state.newPlant.family} placeholder="family" onChange={(event) => this.handleNameChange(event, 'family')} />
+                    <TextField type='text' value={this.state.newPlant.subfamily} placeholder="subfamily" onChange={(event) => this.handleNameChange(event, 'subfamily')} />
+                    <TextField type='text' value={this.state.newPlant.genus} placeholder="genus" onChange={(event) => this.handleNameChange(event, 'genus')} />
+                </div>
+                <br/>
+                <Button variant="contained" color="primary" onClick={this.addNewPlant}>submit</Button>
             </div>
         );
     }
